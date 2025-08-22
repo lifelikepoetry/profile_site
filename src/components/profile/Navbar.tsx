7bc30251c9e08b7f8568ce7cf345e370b38e0a8c
zahnggy01 <1522077114@qq.com>
2025-08-22 16:31:26 +0800

    1

--- src/components/profile/Navbar.tsx
+++ src/components/profile/Navbar.tsx
@@ -9,7 +9,7 @@ const neonderthaw = Neonderthaw({
 })
 
 export default function Navbar() {
-  return (<motion.div className={`bg-amber-50 h-16 flex justify-between items-center px-5 py-3 sticky top-0 z-50`}
+  return (<motion.div className={`h-16 w-full flex justify-between items-center px-5 py-3 fixed top-0 z-50`}
     initial={{ opacity: 0, y: -64, filter: "blur(10px)" }}
     animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
     transition={{ duration: 0.5 }}
@@ -24,23 +24,3 @@ export default function Navbar() {
       </div>
   </motion.div>);
 }
-
-// export default function Navbar() {
-//   return (
-//   // <motion.div className={`bg-amber-50 h-16 flex justify-between items-center px-5 py-3 sticky top-0 z-50`}
-//   //   initial={{ opacity: 0, y: -64, filter: "blur(10px)" }}
-//   //   animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
-//   //   transition={{ duration: 0.5 }}
-//   //   viewport={{ once: false, amount: 0.3 }}
-//   // >
-//   <LiquidGlass style={{height: 64, width: '100vw', display:"flex", justifyContent:"space-between", alignItems:"center", padding: "0 16px", position:"sticky", top: 0, zIndex: 50}}>
-//       <div className={`${neonderthaw.className} text-3xl font-bold`}>LonganZ</div>
-//       <div className="flex gap-5 font-bold">
-//         <a href="#home">Home</a>
-//         <a href="#about">About</a>
-//         <a href="#skills">Skills</a>
-//         <a href="#projects">Projects</a>
-//       </div>
-//     </LiquidGlass>
-// );
-// }
--- src/components/profile/Project.tsx
+++ src/components/profile/Project.tsx
@@ -1,3 +1,8 @@
+"use client"
+import GmailFoot from "@/components/profile/footer-icon/GmailFoot";
+import TwitterFoot from "@/components/profile/footer-icon/TwitterFoot";
+import GithubFoot from "@/components/profile/footer-icon/GithubFoot";
+import WechatFoot from "@/components/profile/footer-icon/WechatFoot";
 import { MorphingDialogBasic } from "./MorphingDialogBasic";
 import { motion, Variants } from "motion/react";
 
@@ -23,7 +28,7 @@ const child: Variants = {
 
 export default function Project() {
     return (
-        <motion.div id="projects" className="flex flex-col gap-4 items-center py-15"
+        <motion.div id="projects" className="h-screen w-full flex flex-col gap-4 items-center py-15 bg-blue-300"
         initial="hidden"
         whileInView="visible"
         viewport={{ once: false, amount: 0.3 }}
@@ -41,6 +46,15 @@ export default function Project() {
                 <MorphingDialogBasic />
             </motion.div>
         </div>
+        <div className="flex gap-4 items-center justify-between border-t-1 border-gray-300 pt-4 mt-40 pb-20 w-full">
+                    <span className="text-gray-500 text-m">&copy; 2025 Longan Zhang. All rights reserved</span>
+                    <div className="gap-4 text-gray-500 cursor-pointer hidden sm:flex">
+                        <GmailFoot width={24} height={24} />
+                        <TwitterFoot width={24} height={24} />
+                        <GithubFoot width={24} height={24} />
+                        <WechatFoot width={24} height={24} />
+                    </div>
+                </div>
     </motion.div>
     )
 }
